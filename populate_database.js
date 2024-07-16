afbe10e3479fa2a856d7c1fd0ce442bf06be9caa
Andy Zheng <zhenghaifeng27@gmail.com>
2024-07-15 21:55:47 -0400

    modified Textsplitter and Doc Loader into funcions

--- populate_database.js
+++ populate_database.js
@@ -1,32 +1,44 @@
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+
 import { Document } from "@langchain/core/documents";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
-async function Main(){
-    const docs = await loader.load();
-    const text = `Hi.\n\nI'm Harrison.\n\nHow? Are? You?\nOkay then f f f f.
-    This is a weird text to write, but gotta test the splittingggg some how.\n\n
-    Bye!\n\n-H.`;
-
-    const docOutput = await splitter.splitDocuments([
-        new Document({ pageContent: docs[0]["pageContent"] }),
-      ]);
-
-
-    console.log(docOutput.slice(0, 3))
-
-};
-
+async function Main() {
+  const doc = await dLoader("data");
+  const splitteddoc = await splitdoc(doc);
+}
 
 // import document load
-const loader = new PDFLoader("data/Per_Diem_Document.pdf", {
-    parsedItemSeparator: "",
-    splitPages: false,
-});
+const dLoader = async function (Path) {
+  try {
+    const directoryLoader = new DirectoryLoader(Path, {
+      ".pdf": (path) => new PDFLoader(path),
+    });
 
-const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    chunkOverlap: 80,
-  });
+    const docs = await directoryLoader.load();
+    return docs;
+  } catch (error) {
+    console.error("Error loading documents:", error);
+    throw error;
+  }
+};
+
+// split document
+const splitdoc = async function (doc) {
+  try {
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 800,
+      chunkOverlap: 80,
+    });
+
+    const splitdocs = await textSplitter.splitDocuments(doc);
+
+    return splitdocs;
+  } catch (error) {
+    console.error("Error Splitting Doc:", error);
+    throw error;
+  }
+};
 
 Main();

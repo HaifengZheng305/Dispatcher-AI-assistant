4e77d765472406d4f60983ac96fcfdda095c3ef9
Andy Zheng <zhenghaifeng27@gmail.com>
2024-07-22 21:38:11 -0400

    Create Query_data to get a response from LLM

---
+++ query_data.js
@@ -0,0 +1,58 @@
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { Ollama } from "@langchain/community/llms/ollama";
+import embed_fn from "./get_embedding_function.js";
+import readline from "node:readline";
+
+const InputPrompt = new PromptTemplate({
+  inputVariables: ["context", "question"],
+  template:
+    "Answer the question based only on the following context: {context}. Answer the question based on the above context: {question}",
+});
+
+async function Main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(``, async (question) => {
+    console.log(await response(question));
+    rl.close();
+  });
+}
+
+/////////////////////////
+// create the response
+//////////////////////////
+const response = async function (queryText) {
+  const vectorStore = new Chroma(embed_fn, {
+    collectionName: "data-collection",
+  });
+
+  const response = await vectorStore.similaritySearch(queryText, 5);
+
+  const context_text = response
+    .map((document) => document["pageContent"])
+    .join("\n\n---\n\n");
+
+  const fromattedInputPrompt = await InputPrompt.format({
+    context: context_text,
+    question: queryText,
+  });
+
+  const ollama = new Ollama({
+    baseUrl: "http://localhost:11434", // Default value
+    model: "llama3", // Default value
+  });
+
+  const stream = await ollama.stream(fromattedInputPrompt);
+
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+
+  return chunks.join("");
+};
+
+Main();
